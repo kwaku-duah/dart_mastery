0cf19db84406c745d34a2673897b9277cc8dd4ab
Sheikhman_Kalderon <duah229@outlook.com>
2026-02-03 22:42:09 +0000

    feat: add variable declaration in dart

---
+++ variables.dart
@@ -0,0 +1,12 @@
+// although dart is a type-sage language, you can declare variables
+// with  type inference (witht the var keyword)
+
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flyObjects = ['Jupiter', 'Saturn', 'Uranus'];
+
+var image = {
+  'tag': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};

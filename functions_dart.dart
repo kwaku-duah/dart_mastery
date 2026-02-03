548f8b612a9a9ecf8c9d2e574a43c983a7b59e6a
Sheikhman_Kalderon <duah229@outlook.com>
2026-02-03 23:05:09 +0000

    feat: add importing libraries, files and packages in dart

---
+++ functions_dart.dart
@@ -0,0 +1,12 @@
+// functions in dart follows the same programming dynamic, return type and function name
+
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+   return fibonacci(n-1) + fibonacci(n-2);
+}
+
+
+void main() {
+var result = fibonacci(20);
+print(result);
+}
---
+++ imports_in.dart
@@ -0,0 +1,8 @@
+// importing core libraries
+import 'dart:math';
+
+//importing libraries from external packages
+import 'package:test/test/dart';
+
+//imporing paths
+import 'path/to/my_other_file.dart';
